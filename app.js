1ec12314c152ad7feb88fc1f3c155047161060e2
RiyaaT <riyathakker1329@gmail.com>
2023-12-09 16:15:26 +0530

    add listing and also can see particular listing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const path = require("path");
 const Listing = require("./models/listing");
 main().then((res)=>{
-    console.log("Done");
+    console.log("Connected to db");
 })
 .catch((err)=>{
     console.log(err)
@@ -15,6 +15,8 @@ async function main(){
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
+//default root
 app.get("/",(req,res)=>{
     res.send("Hi, i am root route");
 });
@@ -30,10 +32,29 @@ await sample.save();
 console.log("Saved");
 res.send("Success");
 });
+//index route
 app.get("/listings",async (req,res)=>{
-  const listing= await Listing.find({});
-    res.render("listings/index.ejs",{listing});
+  const alllisting= await Listing.find({});
+    res.render("listings/index.ejs",{alllisting});
+})
+//form route
+app.get("/listing/new",  (req,res)=>{
+	res.render("listings/new.ejs");
+	})
+//show route
+app.get("/listing/:id", async (req,res)=>{
+	let {id} = req.params;
+	const listing = await Listing.findById(id);
+	res.render("listings/show.ejs",{listing});
+})
+//add route
+app.post("/listing", async (req,res)=>{
+	
+	const newlisting = new Listing(req.body);
+	await newlisting.save();
+	console.log("New listing saved");//printed as js obj with key value pairs
+	res.redirect("/listings");
 })
 app.listen(8080,()=>{
-    console.log("Listening");
+    console.log("Listening on port 8080");
 });

875d7aa6c4a838560ff59f020c777ed8fa7ba398
RiyaaT <riyathakker1329@gmail.com>
2023-12-11 16:50:45 +0530

    navbar and footer

--- models/listing.js
+++ models/listing.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
   title: {
     type: String,
-    required: false,
+    required: true,
   },
   description: String,
   image: {
